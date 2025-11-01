58623aa4a51521e004fa2a7ec63c0225ba6016ff
boullier-lang <mathew_boullier@uri.edu>
2025-11-01 19:50:55 -0400

    assignment 7 finished

---
+++ product_site/js/home.js
@@ -0,0 +1,114 @@
+//11/1/2025
+//Name: Mathew Boullier
+//A simple JS script that swaps between different entries, using conditionals
+
+var last_changed=0;
+
+
+//I've taken a few CSC classes already, so I already know about maps ;)
+//just looked up syntax for it here: https://www.w3schools.com/js/js_maps.asp
+const entries = new Map([
+  ["It (1990)", ["https://upload.wikimedia.org/wikipedia/en/b/b0/It_1990_Promotional_Poster.JPG", "review_pages/review_it-1990.html"]],
+  ["Baldur's Gate 3",["https://upload.wikimedia.org/wikipedia/en/1/12/Baldur%27s_Gate_3_cover_art.jpg", "review_pages/review_bg3.html"] ],
+  ["World of Warcraft", ["images/warcraft_poster.jpg", "review_pages/review_warcraft.html"]],
+  ["Dragonball Z", ["https://m.media-amazon.com/images/M/MV5BN2VlNTdlMzQtYzE5OC00YmYwLTgyZTItYjEzMWY0ZDNjMTJhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", "review_pages/review_dbz.html"]],
+  ["Gattaca", ["https://upload.wikimedia.org/wikipedia/en/d/de/Gattaca_poster.jpg", "review_pages/review_gattaca.html"]],
+  ["The Shawshank Redemption", ["https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg", "review_pages/review_shawshank-redemption.html"]]
+]);
+
+const img = document.getElementById("discover-img");
+const alt = document.getElementById("discover-link");
+
+index = 0;
+
+function set_image(src, alt_name, review_link)
+{
+	last_changed=Date.now();
+	img.src=src;
+	img.alt=alt_name;
+	alt.href=review_link
+	
+}
+
+//Set our image to our first entry
+set_image("https://upload.wikimedia.org/wikipedia/en/b/b0/It_1990_Promotional_Poster.JPG", "It (1990)", "")
+
+function show_entry()
+{
+
+//in order to get our entry name,
+//i followed this documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+entry_names = Array.from(entries.keys())
+our_entry = entries.get(entry_names[index])
+
+set_image(our_entry[0], entry_names[index], our_entry[1])
+
+
+
+}
+
+
+function handle_click(direction)
+{
+	if(direction == 'left')
+	{
+		if(index - 1 == -1)
+		{
+			index = entries.size - 1;
+		}
+		else
+		{
+			index -= 1;
+		}
+		
+		show_entry();
+	}
+	else
+	{
+		if(index+1 == entries.size)
+		{
+			index=0;
+		}
+		else
+		{
+			index+=1;
+		}
+		show_entry();
+
+	}
+
+
+}
+
+
+
+
+
+
+
+
+//Promise object learned from:https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
+//Date time syntax learned from the same source,
+//but I've used it before in other projects
+async function swap_movies()
+{
+
+	//This loop repeats forever, using the Promise object to delay and check the change in time
+	//every second.
+	//I used a while loop in this case because we need it to repeat forever, and since the condition is always
+	//true, a do while() would just be unneccessary.
+	while(true)
+	{
+		//Delay for 1 second
+		await new Promise(r => setTimeout(r, 1000));
+		//Check to see if now - lastchecked is more than 6 seconds
+		if(Date.now() - last_changed > 6000)
+		{
+			handle_click("right")
+		}
+
+	}
+
+}
+
+swap_movies();
--- product_site/js/review.js
+++ product_site/js/review.js
@@ -3,6 +3,54 @@
 //Name: Mathew Boullier
 //Date: 10/26
 //A simple JS script (that took quite a bit of time!) that displays the user's review.
+//Updated 11/1/2025, to be completely modular
+
+//This function will get other scores, add them together, and return the value
+function get_other_scores()
+{
+	var total=0;
+
+	//Get all the paragraphs, since this is where the review is stored.
+	//We know that the first two paragraphs will always be 1) media description, and 2) our review
+	//To get the remaining reviews, we should loop from index 2 to the end
+	paragraphs = document.querySelectorAll("p");
+
+	//This loop goes through each valid paragraph and extracts the score that template users have given.
+	//I used a for loop for this because we are getting indicies of an array, which is usually done in a for-loop.
+	for(var i=2; i<paragraphs.length; i++)
+	{
+		//Now that we have the paragraph text, we need to parse the text to find our review .
+		//We know that all reviews are set up so that the actual value of the review is (x/10). We can use delimiters!
+		var text_content = paragraphs[i].textContent;
+		var start = text_content.indexOf("(");
+		var end = text_content.indexOf(")");
+		var new_txt = text_content.slice(start+1,end);
+		var our_value = Number(new_txt.split("/")[0])
+		total+=our_value;
+		console.log(our_value);
+
+	}
+
+
+	return [total, paragraphs.length-2]
+
+
+
+}
+
+function set_scoring(playerDidReview, score)
+{
+
+  scoring_info = get_other_scores();
+  new_average = ((scoring_info[0]+ score)/ (scoring_info[1]+Number(playerDidReview))).toFixed(1);
+
+  document.getElementsByClassName("center")[2].innerHTML = "<strong> Average Rating: </strong> " + new_average + "/10";
+
+}
+
+//Update our scoring based off page information
+set_scoring(false, 0);
+
 
 function submit_review()
 {
@@ -25,12 +73,11 @@ function submit_review()
   console.log(checkedBackgrounds)
   
   //Update the review page for the user's review!
-  new_average = ((8 + 6 + score)/3).toFixed(1);
+	set_scoring(true, score);
 
-  document.getElementsByClassName("center")[2].innerHTML = "<strong> Average Rating: </strong> " + new_average + "/10"
   const img = document.getElementsByTagName("img")[2];
   
-  img.src="images/logo.png"
+  img.src="../images/logo.png"
   img.alt="your id"
   
   document.querySelector("#your_review").innerHTML = "<strong> You (" + score + "/10) [" + checkedBackgrounds + "] </strong> <br>" + review
